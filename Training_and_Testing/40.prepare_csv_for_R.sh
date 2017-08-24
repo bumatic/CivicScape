9791075f137609e837dc70ccde03847a6532b9f1
sparker6 <sparker6@users.noreply.github.com>
2017-08-24 16:18:26 -0500

    move bin to pg

--- Training_and_Testing/40.prepare_csv_for_R.sh
+++ Training_and_Testing/40.prepare_csv_for_R.sh
@@ -17,44 +17,43 @@ mkdir -p $work $output
 # Bag and bin into the work directory
 # $code/bag_and_bin.sh $input_csv $WC_MODEL_COUNT 1	# use preexisting ones and zeroes files
 
-
 input_csv=$input/${WC_CITY}_train_data.csv
 baseName=$( basename $input_csv | sed 's/.csv//' )
-onesFile=$work/$baseName.ones.csv
-zeroesFile=$work/$baseName.zeroes.csv
+#onesFile=$work/$baseName.ones.csv
+#zeroesFile=$work/$baseName.zeroes.csv
 
-bin_index_day_of_week=$work/bin_index_day_of_week.txt 
-bin_index_month_of_year=$work/bin_index_month_of_year.txt
-bin_index_year=$work/bin_index_year.txt
-bin_index_hournumber=$work/bin_index_hournumber.txt
-bin_index_tract=$input/${WC_CITY}_tracts.csv
+#bin_index_day_of_week=$work/bin_index_day_of_week.txt 
+#bin_index_month_of_year=$work/bin_index_month_of_year.txt
+#bin_index_year=$work/bin_index_year.txt
+#bin_index_hournumber=$work/bin_index_hournumber.txt
+#bin_index_tract=$input/${WC_CITY}_tracts.csv
 
-split_ones_and_zeroes() {
-
-	$code/split_ones_and_zeroes.sh $input_csv $onesFile $zeroesFile
-
-}
-bag() {
-	echo "Bagging $baggedFile from"
-	echo "  $input_csv"
-	echo "  $onesFile"
-	echo "  $zeroesFile"
-	$code/bag.sh $input_csv $onesFile $zeroesFile $baggedFile
-}
+#split_ones_and_zeroes() {
+##
+#	$code/split_ones_and_zeroes.sh $input_csv $onesFile $zeroesFile
+#
+#}
+#bag() {
+#	echo "Bagging $baggedFile from"
+#	echo "  $input_csv"
+#	echo "  $onesFile"
+#	echo "  $zeroesFile"
+#	$code/bag.sh $input_csv $onesFile $zeroesFile $baggedFile
+#}
 # Binning needs files of the possible values for value based binning
-prep_bin() {
-	$helperDir/gen_bin_index_file.sh $bin_index_day_of_week day_of_week 0 6
-	$helperDir/gen_bin_index_file.sh $bin_index_month_of_year month_of_year 1 12
-	$helperDir/gen_bin_index_file.sh $bin_index_year year 2008 2020
-	$helperDir/gen_bin_index_file.sh $bin_index_hournumber hournumber 0 23
-}
-bin() {
-	echo "Binning to $binnedFile"
-	echo "  from $baggedFile"
-	echo "  using these tracts: $bin_index_tract"
-
-	$code/bin.sh $baggedFile $binnedFile $bin_index_day_of_week $bin_index_month_of_year $bin_index_year $bin_index_hournumber $bin_index_tract
-}
+#prep_bin() {
+#	$helperDir/gen_bin_index_file.sh $bin_index_day_of_week day_of_week 0 6
+#	$helperDir/gen_bin_index_file.sh $bin_index_month_of_year month_of_year 1 12
+#	$helperDir/gen_bin_index_file.sh $bin_index_year year 2008 2020
+#	$helperDir/gen_bin_index_file.sh $bin_index_hournumber hournumber 0 23
+#}
+#bin() {
+#	echo "Binning to $binnedFile"
+#	echo "  from $baggedFile"
+#	echo "  using these tracts: $bin_index_tract"
+#
+#	$code/bin.sh $baggedFile $binnedFile $bin_index_day_of_week $bin_index_month_of_year $bin_index_year $bin_index_hournumber $bin_index_tract
+#}
 
 #if [ $WC_CITY = "seattle_wa" ] || [ $WC_CITY = "boston_ma" ]; then
 #	input_csv=$input/${WC_CITY}_train_data.csv
@@ -67,14 +66,15 @@ bin() {
 #	echo "City not implemented"
 #fi
 
-split_ones_and_zeroes
-prep_bin
+#split_ones_and_zeroes
+#prep_bin
 
 for i in $(seq 1 $WC_MODEL_COUNT); do
 	echo "i: $i"
-	baggedFile=${work}/$baseName.$i.bagged.csv
-	binnedFile=${output}/$baseName.$i.binned.csv
-	bag
-	bin
+	cp ${input_dir}/${WC_CITY}_train_data.bagged.${i}.csv ${output}/$baseName.$i.binned.csv
+	#baggedFile=${work}/$baseName.$i.bagged.csv
+	#binnedFile=${output}/$baseName.$i.binned.csv
+	#bag
+	#bin
 
 done
